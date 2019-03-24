a84c20c9269daec86346e0a2a80879a6be3471c4
hayri simsek <simsekha@hotmail.com>
2019-03-24 12:59:45 +0300

    First Commit

---
+++ src/com/simsek/hayri/model/Calzone.java
@@ -0,0 +1,35 @@
+package com.simsek.hayri.model;
+
+import com.simsek.hayri.abstracts.Pizza;
+
+public class Calzone extends Pizza {
+
+	private final boolean sauceInside;
+
+	public static class Builder extends Pizza.Builder<Builder> {
+
+		private boolean sauceInside = false;
+
+		public Builder sauceInside() {
+			sauceInside = true;
+			return this;
+		}
+
+		@Override
+		public Calzone build() {
+			return new Calzone(this);
+		}
+
+		@Override
+		protected Builder self() {
+			return this;
+		}
+
+	}
+
+	private Calzone(Builder builder) {
+		super(builder);
+		sauceInside = builder.sauceInside;
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author simse
+ *
+ */
+module BuilderPattern {
+}
